26680ebc309495485c9d60c61b43559df49d1737
ANTONIO ADAIR CABREIRA NETO <antonio.neto05@cs.up.edu.br>
2025-03-31 19:59:58 -0300

    Teste

---
+++ Projeto.Antonio.31/Program.cs
@@ -0,0 +1,16 @@
+﻿{
+    int numero = 10;
+    int numero_2 = 5;
+    Console.WriteLine("Quanto é: " + numero + " + " + numero_2 + "?");
+
+    int resposta = Convert.ToInt32(Console.ReadLine());
+
+    if (resposta == 15)
+    {
+        Console.WriteLine("Parabéns você acertou!");
+    }
+    else
+    {
+        Console.WriteLine("Tente Novamente!");
+    }
+}
